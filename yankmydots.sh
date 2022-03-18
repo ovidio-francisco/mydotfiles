f17be6b7169872dd6a937be08f76b5daaa6b9ee4
Prof. Ovídio <prof@debian.168.15.14>
2022-03-18 11:45:11 -0300

    Trashing some stuffs

--- yankmydots.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-echo "Copying files"
-cp -v ~/.taskrc ./.taskrc
-cp -v ~/.vimrc ./.vimrc
-cp -v ~/.bashrc ./.bashrc
-cp -v ~/.vim/syntax/markdown.vim ./markdown.vim
-cp -v ~/.vim/bundle/lightline.vim/autoload/lightline/colorscheme/jellybeans.vim jellybeans.vim
